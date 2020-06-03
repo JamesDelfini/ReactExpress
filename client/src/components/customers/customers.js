7dafc5dfae50751eda91fed286ac3ebbee323f1a
Dennis James Matildo <dj.matildo@gmail.com>
2020-06-04 02:36:22 +0800

    Setup the React View for Customers

---
+++ client/src/components/customers/customers.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './customers.css';
+
+class Customers extends Component {
+  render () {
+    return (
+      <div className="App">
+        <h2>Customers</h2>
+      </div>
+
+      
+    );
+  }
+}
+
+export default Customers;
